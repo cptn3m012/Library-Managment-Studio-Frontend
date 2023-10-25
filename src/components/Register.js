abb98e9199c8b9efe245225cd0ab5d35c6ec34df
Filip Habryn <filip.habryn@gmail.com>
2023-10-25 12:48:37 +0200

    tailwind test and connection to the backend

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,32 +1,25 @@
-export default function Login() {
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = new FormData(event.target);
-
-    const response = await fetch('http://localhost:5000/login', {
-        method: 'POST',
-        headers: {
+export default function RegistrationForm() {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = new FormData(event.target);
+    
+        const response = await fetch('http://localhost:5000/register', {
+          method: 'POST',
+          headers: {
             'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          first_name: data.get('first_name'),
+          },
+          body: JSON.stringify({
+            first_name: data.get('first_name'),
             password: data.get('password'),
-        }),
-    });
-
-    const result = await response.json();
-    console.log(result);
-};
+          }),
+        });
+    
+        const result = await response.json();
+        console.log(result);
+      };
+    
     return (
       <>
-        {/*
-          This example requires updating your template:
-  
-          ```
-          <html class="h-full bg-white">
-          <body class="h-full">
-          ```
-        */}
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
@@ -35,21 +28,21 @@ export default function Login() {
               alt="Your Company"
             />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Logowanie do systemu
+              Create your account
             </h2>
           </div>
   
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" method="POST" onSubmit={handleSubmit}>
               <div>
-                <label htmlFor="first_name" className="text-left block text-sm font-medium leading-6 text-gray-900">
-                  Login/Adres email
+                <label htmlFor="first_name" className="block text-sm font-medium leading-6 text-gray-900">
+                  first_name
                 </label>
                 <div className="mt-2">
                   <input
                     id="first_name"
                     name="first_name"
-                    type="first_name"
+                    type="text"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
@@ -57,22 +50,31 @@ export default function Login() {
               </div>
   
               <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Hasło
-                  </label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Zapomniałeś/aś hasła?
-                    </a>
-                  </div>
-                </div>
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
                 <div className="mt-2">
                   <input
                     id="password"
                     name="password"
                     type="password"
-                    autoComplete="current-password"
+                    autoComplete="new-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <label htmlFor="confirm-password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Confirm Password
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="confirm-password"
+                    name="confirm-password"
+                    type="password"
+                    autoComplete="new-password"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
@@ -84,12 +86,19 @@ export default function Login() {
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Zaloguj się
+                  Sign Up
                 </button>
               </div>
             </form>
+  
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Already have an account?{' '}
+              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                Sign in
+              </a>
+            </p>
           </div>
         </div>
       </>
     )
-}
+  }
