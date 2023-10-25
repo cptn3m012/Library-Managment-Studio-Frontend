c36e8ce2f32175e0846742921072a0b43d69ce45
Filip Habryn <filip.habryn@gmail.com>
2023-10-25 12:46:09 +0200

    improving the file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Login from './components/Login';
+import RegistrationForm from './components/Register';
 
 function App() {
   return (
       <div className="App">
+          <RegistrationForm />
           <Login />
       </div>
   );
